0d0740add20f56d22e4cd8ab0a16e6313f8fbcff
cmjoyce <39970335+cmjoyce@users.noreply.github.com>
2022-10-19 17:11:53 -0400

    updating ahs and nfhs for linking

--- AHS/ahslink.R
+++ AHS/ahslink.R
@@ -8,7 +8,7 @@ library(tidyverse)
 
 women <- read_dta("women.dta", col_select = c(1:31, 46:49, 54:55, 85:104, 122:155))
 
-wps <- read_dta("wps.dta", col_select = c(1:49, 59:62, 93, 126:127, 218:219, 249:264, 286:319))
+wps <- read_dta("wps.dta", col_select = c(1:49, 59:62, 93, 126:127, 218:219, 249:267, 286:319))
 #names(wps)
 
 names(women)
@@ -58,11 +58,8 @@ wps <- wps %>% add_count(caseid)
 
 
 table(wps$n)
-check <- subset(wps, n > 3)
+#check <- subset(wps, n > 3)
 
-
-# start with pregnancies. Limit to most recent pregnancy
-wps$prev_stillbirth <- ifelse(wps$out_come_of_preg == 2, 1, 0)
 
 #making most recent birth designation
 #wps <- wps %>% mutate(recent_birth = top_n(1, yob) ~ 1,
@@ -149,51 +146,288 @@ check <- semi_join(wps_id, ahs_id, by = "caseid")
 names(wps)
 wps <- wps %>% relocate(caseid, .before = state)
 
-wps_check <- wps %>% filter(n > 7)
-
 #WPS -- filtering out any cases without PSUs (~10 %)
 
 wps_full <- wps %>% filter(!is.na(psu))
 
+#filtering out cases without member identity (~4k)
+wps_full <- wps_full %>% filter(!is.na(member_identity))
+
+
 table(wps_full$n)
-wps_check <- wps_full %>% filter(n > 6)
 
 #need a most recent outcome variable combining births + abortions so can take most recent occurrence
 #filtering out variables not needed
 names(wps_full)
 
-wps_full <- wps_full %>% select(caseid, state, district, rural, stratum_code, psu, hhld_id, result_of_interview, date_of_intr, month_of_intr, year_of_intr,
+wps_full <- wps_full %>% select(caseid, state, district, rural, stratum_code, psu, hhld_id, member_identity, result_of_interview, date_of_intr, month_of_intr, 
+                                year_of_intr,
                                 intvw_yrmo, round, age, marital_status, ever_conceived, no_of_times_conceived, age_at_first_conception, delivered_any_baby, 
                                 born_alive_total, surviving_total, mother_age_when_baby_was_born, outcome_pregnancy, preg_flag, out_come_of_preg, month_of_abortion,
                                 year_of_abortion, abort_yrmo, abortion_month, anc_status, ultrsound_status, abortion_performed, abortion_performed_by, 
                                 reason_for_abortion, kind_of_birth, previous_current_diff, dob, mob, yob, birth_yrmo, gender, no_of_anc, no_of_months_first_anc,
                                 source_of_anc, where_del_took_place, who_conducted_del_at_home, check_up_with_48_hours_of_del, is_any_fp_methos_used, fp_method_used,
-                                sex, usual_residance, date_of_birth, month_of_birth, year_of_birth, mom_birth_yrmo, religion, social_group_code, house_structure,
+                                sex, usual_residance, date_of_birth, month_of_birth, year_of_birth, mom_birth_yrmo, religion, social_group_code, highest_qualification, 
+                                house_structure,
                                 owner_status, drinking_water_source, is_water_filter, water_filteration, toilet_used, iscoveredbyhealthscheme, healthscheme_1,
-                                healthscheme_2, wt, as, as_binned, nfhs4_census2011_district_id, n, prev_stillbirth)
+                                healthscheme_2, wt, as, as_binned, nfhs4_census2011_district_id)
 
 #making wps outcome variable. If outcome is 3 or 4 (induced and spontaneous abortion) we use use year of abortion. For outcome 1 and 2 we use use year of birth.
 
-wps_full <- wps_full %>% mutate(outcome_year = case_when(out_come_of_preg < 3 ~ yob,
-                                               out_come_of_preg > 2 ~ year_of_abortion,
-                                               TRUE ~ NA_real_))
+#too much memorty to run, going as if/else statement instead
+#wps_full <- wps_full %>% mutate(outcome_year = case_when(out_come_of_preg < 3 ~ yob,
+#                                               out_come_of_preg > 2 ~ year_of_abortion,
+#                                               TRUE ~ NA_real_))
 
-wps_full$outcome <- ifelse(wps_full$out_come_of_preg < 3, wps_full$yob, NA)  
-wps_full$outcome <- ifelse(wps_full$out_come_of_preg > 2, wps_full$year_of_abortion, wps_full$outcome) 
+wps_full$outcome_year <- ifelse(wps_full$out_come_of_preg < 3, wps_full$yob, NA)  
+wps_full$outcome_year <- ifelse(wps_full$out_come_of_preg > 2, wps_full$year_of_abortion, wps_full$outcome_year) 
 
 wps_full$outcome_month <- ifelse(wps_full$out_come_of_preg < 3, wps_full$mob, NA)
 wps_full$outcome_month <- ifelse(wps_full$out_come_of_preg > 2, wps_full$month_of_abortion, wps_full$outcome_month)
 
 #dropping all NAs for outcome year. Means it was missing either the type of preg outcome OR the year that the outcome occurred. 
 
-wps_full <- wps_full %>% filter(!is.na(outcome))
-
-#renaming outcome to be outcome year
-wps_full <- rename(wps_full, outcome_year = outcome)
-
-wpscheck <- wps_full %>% select(caseid, out_come_of_preg, mob, yob, month_of_abortion, year_of_abortion, outcome_year, outcome_month)
+wps_full <- wps_full %>% filter(!is.na(outcome_year))
 
 #writing csv
 #write.csv(wps_full, "wpsfull.csv")
 
+#wps_full <- read.csv("wpsfull.csv")
+wps_full <- wps_full %>% select(-c(X))
+
+wps_full <- wps_full %>% group_by(caseid)
+
 names(dlhs_nfhs_preg)
+
+table(wps_full$round, wps_full$year_of_intr)
+
+
+#filtering out only most recent pregnancy outcomes to match dlhsnfhs_preg.
+
+#checking to make sure it's grouped
+is_grouped_df(wps_full) #Yes
+
+#making previous stillbirth variable before limiting to most recent outcome
+# start with pregnancies. Limit to most recent pregnancy
+
+wps_full <- wps_full %>% mutate(stillbirths = length(which(out_come_of_preg ==2)))
+
+# making most recent termination if there was termination before most recent birth
+
+wps_full <- wps_full %>% mutate(nonlivebirths = length(which(out_come_of_preg > 1)))
+
+wps_full$recent_mab_year <- ifelse(wps_full$out_come_of_preg == 2, wps_full$yob, NA)
+wps_full$recent_mab_year <- ifelse(wps_full$out_come_of_preg == 3, wps_full$year_of_abortion, wps_full$recent_mab_year)
+wps_full$recent_mab_year <- ifelse(wps_full$out_come_of_preg == 4, wps_full$year_of_abortion, wps_full$recent_mab_year)
+
+# need to assign type of most recent termination to group. Probably need to do unique rows first, then:
+# by group most recent outcome -- if termination what kind and DONE.
+# if most recent outcome is birth then find most recent termination after.
+# if an
+
+#wps_full <- wps_full %>% group_by(caseid) %>% mutate(last_even_flag = case_when(outcome_year == max(outcome_year ) ~ 1,
+                                                                                TRUE ~ 0))
+
+# In NFHS and DLHS we have most recent birth outcome (outcome), and then, if there was at least 1 termination before the most recent outcome what that was.
+# leaving for now because we are not including how many pregnancies there were between most recent pregnancy and outcome
+
+#check2 <- check2 %>% mutate(mab = miscarriage_abortion_stillbirth[recent_term == 1L])
+
+#creating previous stillbirths logic: If more than one stillbirth == YES, if == 1 stillbirth & year of stillbirth != latest outcome date == YES
+# if == 1 stillbirth && stillbirth date == latest outcome date == NO previous stillbirth
+
+#starting with if >1 stillbirth
+
+wps_full$prev_stillbirth <- ifelse(wps_full$stillbirths > 1, 1, 0)
+
+#Need to note index pregnancy (last most recent) and see if there was a stillbirth before index preg, if so stillbirth. Current code not working.
+#Need year of most recent stillbirth if ANY stillbirths at all. 
+
+wps_full$stillbirth_year <- ifelse(wps_full$out_come_of_preg == 2, wps_full$yob, NA)
+
+## THIS CODE RUN ON CLUSTER. TOO MUCH FOR DESKTOP ##
+wps_full <- wps_full %>% group_by(caseid) %>% mutate(maxyearsb = max(stillbirth_year, na.rm = TRUE))
+## END CODE RUN ON CLUSTER ##
+
+wpssb <- read.csv("wpssb.csv") #merge this wps_full to get sb years for groups
+
+wpssb <- wpssb %>% select(-c(X))
+
+wps_allsb <- read.csv("wps_all_sb.csv")
+
+
+
+wpsstill <- wpssb %>% select(c(caseid, year_of_intr, round, out_come_of_preg, outcome_year, maxyearsb))
+
+wpssb$caseid <- as.character(wpssb$caseid)
+wps_full$state <- as.integer(wps_full$state)
+wps_full$district <- as.integer(wps_full$district)
+wps_full$psu <- as.integer(wps_full$psu)
+
+wps_preg <- left_join(
+  wps_full,
+  wpssb,
+#  by = c("caseid", "year_of_intr", "round", "ever_conceived",
+#         "out_come_of_preg", "yob", "n", "outcome_year","stillbirths", "prev_stillbirth", "stillbirth_year")
+)
+
+#create previous stillbirth variable
+wps_preg <- wps_preg %>% mutate(previous_sb = case_when(maxyearsb < outcome_year ~ 1,
+                                                        TRUE ~ 0))
+
+#write.csv(wps_preg, "wps_preg.csv")
+wps_preg <- read.csv("ahs_preg.csv")
+
+#grouping by caseid
+
+wps_preg <- wps_preg %>% group_by(caseid)
+wps_preg <- wps_preg %>% add_count(caseid)
+
+#wpssb <- wps_full %>% filter(stillbirths > 0)
+#wpssb <- wpssb %>% mutate(checkmax = max(stillbirth_year, na.rm = TRUE))
+
+#wpssb <- wpssb %>% mutate(maxoutcome = max(outcome_year))
+
+#mutate(marital_status = c7[femaleheadspouse == 1L])
+
+
+
+# Limiting to most recent outcome -----------------------------------------
+
+ahs_preg <- ahs_preg %>% arrange(desc(outcome_year)) %>% slice(1)
+
+#write.csv(ahs_preg, "ahs_preg.csv")
+ahs_preg <- read.csv("ahs_preg.csv")
+
+# Combining with dlhs_nfhs_preg -------------------------------------------
+
+names(dlhsnfhs_preg_mothercov)
+
+names(ahs_preg)
+
+#dropping ahs_preg variables we don't need
+ahs_preg <- ahs_preg %>% select(-c(hhld_id, member_identity, result_of_interview, intvw_yrmo, abort_yrmo, n, stillbirths, prev_stillbirth, stillbirth_year, 
+                                   maxyearsb, nn))
+
+dlhsnfhs_preg_mothercov <- dlhsnfhs_preg_mothercov %>% relocate(state, .before = district)
+
+ahs_preg$survey <- c("ahs")
+
+#adding in education variable from full WPS dataset. Matching and merging.
+wps_educ <- wps %>% select(c(caseid, state, district, rural, stratum_code, psu, date_of_intr, month_of_intr, year_of_intr, round, age, marital_status,
+                             ever_conceived, no_of_times_conceived, age_at_first_conception, delivered_any_baby, born_alive_total, 
+                             surviving_total, mother_age_when_baby_was_born, outcome_pregnancy, preg_flag, out_come_of_preg, month_of_abortion,
+                             year_of_abortion, abortion_month, anc_status, ultrsound_status, abortion_performed, abortion_performed_by,
+                             reason_for_abortion, kind_of_birth, previous_current_diff, dob, mob, yob, birth_yrmo, gender, no_of_anc, 
+                             no_of_months_first_anc, source_of_anc, where_del_took_place, who_conducted_del_at_home, check_up_with_48_hours_of_del,
+                             is_any_fp_methos_used, fp_method_used, sex, usual_residance, date_of_birth, month_of_birth, year_of_birth, mom_birth_yrmo,
+                             religion, social_group_code, highest_qualification, house_structure, owner_status, drinking_water_source,
+                             is_water_filter, water_filteration, toilet_used, iscoveredbyhealthscheme, healthscheme_1, healthscheme_2,
+                             wt, as, as_binned, nfhs4_census2011_district_id))
+
+
+ahs_preg$state <- str_pad(ahs_preg$state, 2, "left", "0")
+ahs_preg$district <- str_pad(ahs_preg$district, 2, "left", "0")
+#stratum code is 0/1/2, leaving as is
+#making psu 3 numbers
+ahs_preg$psu <- str_pad(ahs_preg$psu, 3, "left", "0")
+
+
+
+ahs_preg_educ <- left_join(
+  ahs_preg,
+  wps_educ
+)
+
+
+#picking out distinct (some duplicates in wps that we removed when picking most recent outcome. Duplicates seem to be mostly round 1 and 2)
+nrow(ahs_preg)
+nrow(ahs_preg_educ)
+nrow(distinct(ahs_preg_educ))
+
+ahs_preg_educ <- distinct(ahs_preg_educ)
+
+
+ahs_preg_match <- ahs_preg_educ %>% select(c(caseid, state, district, psu, out_come_of_preg, previous_sb, survey, year_of_abortion, yob, wt, year_of_intr, age,
+                                  rural, religion, social_group_code, highest_qualification,drinking_water_source, toilet_used, healthscheme_1, 
+                                  healthscheme_2, age_at_first_conception,
+                                  born_alive_total, no_of_months_first_anc, no_of_anc, kind_of_birth, previous_current_diff, where_del_took_place, who_conducted_del_at_home,
+                                  check_up_with_48_hours_of_del, abortion_month, fp_method_used, as, nfhs4_census2011_district_id))
+
+
+#making health insurance variables
+ahs_preg_match$esis <- ifelse(ahs_preg_match$healthscheme_1 == 1 | ahs_preg_match$healthscheme_2 == 1, 1, 0)
+ahs_preg_match$rsby <- ifelse(ahs_preg_match$healthscheme_1 == 2 | ahs_preg_match$healthscheme_2 == 2, 1, 0)
+ahs_preg_match$othergov <- ifelse(ahs_preg_match$healthscheme_1 == 3 | ahs_preg_match$healthscheme_2 == 3, 1, 0)
+ahs_preg_match$reimburse <- ifelse(ahs_preg_match$healthscheme_1 == 4| ahs_preg_match$healthscheme_2 == 4, 1, 0)
+ahs_preg_match$chip <- ifelse(ahs_preg_match$healthscheme_1 == 5| ahs_preg_match$healthscheme_2 == 5, 1, 0)
+ahs_preg_match$mediclaim <- ifelse(ahs_preg_match$healthscheme_1 == 6 | ahs_preg_match$healthscheme_2 == 6, 1, 0)
+ahs_preg_match$other_insurance <- ifelse(ahs_preg_match$healthscheme_1 == 7 | ahs_preg_match$healthscheme_2 == 7, 1, 0)
+
+ahs_preg_match <- ahs_preg_match %>% select(-c(healthscheme_1, healthscheme_2))
+
+names(dlhsnfhs_preg_mothercov)
+names(ahs_preg_match)
+
+#dropping dlhsnfhs_preg_mothercov variables not in ahs
+dlhsnfhs_preg_mothercov <- dlhsnfhs_preg_mothercov %>% select(-c(ever_terminated, month_last_terminated, year_last_terminated, caste,
+                                                                 fp_future, bpl, health_ins, other_terminations, miscarriage_abortion_stillbirth))
+
+
+
+
+# fixing district to match NFHS -------------------------------------------
+
+
+
+
+
+
+#looking at most recent year of outcome. Will also need to further filter by preg_flag to be most recent pregnancy as for some reaosn 
+#there are some instances of multiple births per year
+
+wps_try <- wps_full %>% group_by(caseid) %>% top_n(1, outcome_year)
+
+wps_try <- wps_try %>% filter(preg_flag == 1)
+
+wps_try <- wps_try %>% add_count(caseid)
+table(wps_try$nn)
+
+
+#looking at those with more than one preg
+check <- wps_try %>% filter(nn > 1)
+
+# Removing duplicates that are likely mistakes in entering rounds. I.e., if year of interview is 2011 but round is 3. 
+# Round 1 was collected July 2010 - March 2011, Round 2 was collected Oct 2011 - April 2012,
+# Round 3 was collected Nov 2012 - May 2013
+
+# Removing all round 3s from 2010 & 2011
+# Removing all Round 2s from 2010 & 2013
+# Removing all round 1s from 2012
+
+length(which(wps_try$round == 3 & wps_try$year_of_intr == 2010))
+length(which(wps_try$round == 3 & wps_try$year_of_intr == 2011))
+
+#filtering out NAs for year of interview
+wps_try <- wps_try %>% filter(!is.na(year_of_intr))
+
+#now filtering out rounds in wrong year and making NA
+wps_try$round1 <- ifelse(wps_try$round == 3 & wps_try$year_of_intr < 2012, NA, wps_try$round)
+wps_try$round1 <- ifelse(wps_try$round == 2 & wps_try$year_of_intr == 2010, NA, wps_try$round1)
+wps_try$round1 <- ifelse(wps_try$round == 2 & wps_try$year_of_intr == 2013, NA, wps_try$round1)
+wps_try$round1 <- ifelse(wps_try$round == 1 & wps_try$year_of_intr == 2012, NA, wps_try$round1)
+
+#now filtering out these NAs
+wps_try <- wps_try %>% filter(!is.na(round1))
+
+wps_try1 <- wps_try %>% select(-c(nn, round1))
+wps_try1 <- wps_try1 %>% add_count(caseid)
+
+check <- wps_try1 %>% filter(nn > 1)
+
+#should only be 1 pregnancy (most recent for caseid). Filtering out the ~1060 with more than one preganncy kept. On check it looked like errors (i.e. multiple entires for same pregnancy)
+
+ahs_preg <- wps_try1 %>% filter(nn == 1)
+
+names(ahs_preg)
+
